8f775ce0fcc1444c34b8fbcc4f755862420cb61f
Pranav Khurana <49186188+PKhurana09@users.noreply.github.com>
2023-03-15 15:48:59 +1100

    New Management functions added

---
+++ main.py
@@ -0,0 +1,25 @@
+import random
+import datetime
+
+def management():
+    print('\t\t\t\tWELCOME TO KHURANA NIWAS')
+    print('1.MAKE A BOOKING\n2.ROOM INFO\n3.SERVICES\n4.PAYMENT\n5.RECORDS')
+
+    var = int(input('Please enter your choice'))
+
+    if var == 1:
+        pass
+    elif var == 2:
+        pass
+    elif var == 3:
+        pass
+    elif var == 4:
+        pass
+    elif var == 5:
+        pass
+    else:
+        print('Wrong choice.\n Try Again')
+        management()
+    
+
+management()
