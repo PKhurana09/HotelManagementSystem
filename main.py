8c0897ac1b80fafb9d7436094fca073b4ed668f1
Pranav Khurana <49186188+PKhurana09@users.noreply.github.com>
2023-03-15 15:52:36 +1100

    New Functions added

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 import datetime
 
+# Global variables:
+rooms = []
+
 def management():
     print('\t\t\t\tWELCOME TO KHURANA NIWAS')
     print('1.MAKE A BOOKING\n2.ROOM INFO\n3.SERVICES\n4.PAYMENT\n5.RECORDS')
@@ -8,18 +11,41 @@ def management():
     var = int(input('Please enter your choice'))
 
     if var == 1:
+        makeBooking()
         pass
     elif var == 2:
+        service()
         pass
     elif var == 3:
+        roomInfo()
         pass
     elif var == 4:
+        payment()
         pass
     elif var == 5:
+        records()
         pass
     else:
-        print('Wrong choice.\n Try Again')
-        management()
+        print('Wrong choice.\nTry Again')
+        
     
 
 management()
+
+
+def makeBooking():
+    pass
+
+def service():
+    pass
+
+def roomInfo():
+    
+
+    pass
+
+def payment():
+    pass
+
+def records():
+    pass
